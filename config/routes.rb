c0ba7836ecaacda9ddf2b94faae32a6915427216
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-09-09 18:58:50 +0900

    声の投稿機能関係

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: 'gifts#index'
   resources :users
   resources :gifts
-
-  root to: 'gifts#index'
-
+  resources :koes, only: [:index, :create]
+ 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_095206) do
+ActiveRecord::Schema.define(version: 2020_09_08_054059) do
+
+  create_table "koes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "encrypted_password", default: "", null: false
