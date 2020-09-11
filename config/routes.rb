93b7b5132542ebdf5a2a11d134c49a939908df40
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-09-12 00:27:15 +0900

    fontawesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'gifts#index'
-  resources :users
+  resources :users, only: :show
   resources :gifts
-  resources :koes, only: [:index, :create]
+  resources :koes
+  # only: [:index, :create, :show]
 
 
  
